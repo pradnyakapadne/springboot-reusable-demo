9c106f1195574b54a9a5693476946af9d7b8e20c
Pradnya Kapadne <your-email@example.com>
2026-06-09 13:00:48 +0530

    trigger

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from GitHub Reusable Workflow";
+        return "Deployment Success via GitHub Actions";
     }
 }
